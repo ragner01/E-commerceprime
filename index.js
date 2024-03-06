3e1738dc8a716329396ce25690340f8fe9d4a806
ragner01 <alimiomotola20@gmail.com>
2024-03-06 12:26:27 +0100

    MongoDB setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const connectDB = require('./config/dbConfig')
+const mongoose = require('mongoose')
 
 // initialize an express object
 const app = express()
 // for json data
 app.use(express.json())
+
+//db config
+connectDB()
 
 // for json data
 app.use(express.json())
@@ -13,6 +18,10 @@ app.use(express.json())
 const PORT = process.env.PORT || 5890
 
 //connect
+mongoose.connection.once('open', () => { 
+    console.log('database connection established')
+
 app.listen(PORT, () => {
     console.log(`server is running on port http://localhost:${PORT}`)
+})
 })
